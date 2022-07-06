1ea864377f5c51d6ae262586a35496c49542e682
unknown <xogk1128@naver.com>
2022-07-06 16:19:39 +0900

    문제3

---
+++ function/pro3/소스.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int calc(int N) {
+	int r1, r2, r3, cnt = 99;
+	if (N < 100)printf("%d",N);
+	else {
+		for (int i = 100; i <= N; i++) {
+			r1 = N / 100;
+			r2 = (N / 10) % 10;
+			r3 = N % 10;
+			if (r1 - r2 == r2 - r3)cnt++;
+		}
+		printf("%d", cnt);
+	}
+}
+
+int main() {
+	int N, sum=0;
+	scanf("%d", &N);
+	calc(N);
+}
